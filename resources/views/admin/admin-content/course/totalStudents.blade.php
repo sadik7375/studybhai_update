28437cdc3354b2d10027937e6f7c6f6829d6382e
SadiaRidita <sadiaafrinhridita@gmail.com>
2024-01-29 12:12:24 +0600

    changed in partial payment

--- resources/views/admin/admin-content/course/totalStudents.blade.php
+++ resources/views/admin/admin-content/course/totalStudents.blade.php
@@ -1,60 +1,37 @@
 @extends('admin.admin-panel.main')
 
-    @section('content')
-        <div class="container">
-            <h1>Total Students</h1>
+@section('content')
+    <div class="container">
+        <h1>Total Students</h1>
 
-            <table class="table">
-                <thead>
+        <table class="table table-bordered">
+            <thead class="thead-dark">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Status</th>
+                <!-- Add more table headers as needed -->
+            </tr>
+            </thead>
+            <tbody>
+            <?php $serial = 0 ?>
+            @forelse($orders as $order)
                 <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Phone</th>
-                    <th>Email</th>
-                    <th>Status</th>
-                    <th>Enrolled Courses</th>
-                    <th>Registration Date</th>
-                    <th>Enroll Date</th>
-                    <th>Completion Date</th>
+                    <td>{{ $serial += 1 }}</td>
+                    <td>{{ optional($order['user'])->name }}</td>
+                    <td>{{ $order['phone'] }}</td>
+                    <td>{{ optional($order['user'])->email }}</td>
+                    <td>{{ $order['status'] }}</td>
+                    <!-- Add more table data cells as needed -->
                 </tr>
-                </thead>
-                <tbody>
-                <!-- Loop through students data to populate the table -->
-{{--                @foreach ($students as $student)--}}
-{{--                    <tr>--}}
-{{--                        <td>{{ $student->id }}</td>--}}
-{{--                        <td>{{ $student->name }}</td>--}}
-{{--                        <td>{{ $student->phone }}</td>--}}
-{{--                        <td>{{ $student->email }}</td>--}}
-{{--                        <td>{{ $student->enrolled_courses }}</td>--}}
-{{--                        <td>{{ $student->registration_date }}</td>--}}
-{{--                        <td>{{ $student->enroll_date }}</td>--}}
-{{--                        <td>{{ $student->completion_date }}</td>--}}
-{{--                    </tr>--}}
-{{--                @endforeach--}}
+            @empty
                 <tr>
-                    <td>1</td>
-                    <td>Ridita</td>
-                    <td>01879589416</td>
-                    <td>ridita@gmail.com</td>
-                    <td>enrolled</td>
-                    <td>Machine Learning</td>
-                    <td>23/01/24</td>
-                    <td>23/01/24</td>
-                    <td>23/08/24</td>
+                    <td colspan="5">No students found</td>
                 </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Sadia</td>
-                    <td>01879589416</td>
-                    <td>ridita@gmail.com</td>
-                    <td>enrolled</td>
-                    <td>Machine Learning</td>
-                    <td>23/01/24</td>
-                    <td>23/01/24</td>
-                    <td>23/08/24</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    @endsection
+            @endforelse
+            </tbody>
+        </table>
+    </div>
+@endsection
