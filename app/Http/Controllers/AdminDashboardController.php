59d5b419d8457c54244a1ca60da9a4d2c9759c72
sadik7375 <wahidsadik7375@gmail.com>
2024-02-12 14:24:36 +0600

    all

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -1,24 +1,25 @@
 <?php
 namespace App\Http\Controllers;
 
-use App\Models\admin\Course;
+//use App\Models\admin\Course;
 use App\Models\Orders;
 use App\Models\Team;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 
 class AdminDashboardController extends Controller
 {
     public function index()
     {
-//        $coursesCount = Course::count();
+        $coursesCount = DB::table('courses')->count();
         $studentsCount = Orders::count();
         $trainersCount = Team::count();
         $orders = Orders::all(); // Or use your own query to fetch orders
 
         $totalRevenue = $orders->sum('amount');
 
-        return view('dashboard.adminindex', compact( 'studentsCount', 'trainersCount', 'totalRevenue', 'orders'));
+        return view('dashboard.adminindex', compact('coursesCount', 'studentsCount', 'trainersCount', 'totalRevenue', 'orders'));
     }
 }
